513896d2817535342289bd84517f0e5a12de9aa9
gong023 <gon.gong.gone@gmail.com>
2014-07-16 01:52:12 +0900

    update closure

--- closure.rs
+++ closure.rs
@@ -1,3 +1,5 @@
+extern crate debug;
+
 fn apply(i: int, f: |int|->int) -> int {
     f(i)
 }
@@ -10,15 +12,27 @@ fn apply_with_10(f: |int|->int) -> int {
     f(10)
 }
 
+fn rpc_call(p: proc(v: int)) {
+    p(10)
+}
+
+fn each(v: &[int], f: |v: &int|) {
+    let mut i = 0;
+    while i < v.len() {
+        f(&v[i]);
+        i += 1;
+    }
+}
+
 fn main() {
     let should_four = apply(2, { |x| x * x });
-    assert_eq!(should_four, 4i);
+    assert_eq!(4i, should_four);
 
     let closure = || println!("Hello");
     apply_with_none(closure);
 
     let should_twenty = apply_with_10({ |i| i * 2 });
-    assert_eq!(should_twenty, 20i);
+    assert_eq!(20i, should_twenty);
 
     let mut max = 0i;
     {
@@ -27,8 +41,14 @@ fn main() {
             find_max(*x);
         }
     }
-    println!("max={}", max);
+    assert_eq!(3i, max);
 
     [1, 2, 3].iter().map(|x| if *x > max { max = *x });
-    println!("max={}", max);
+    assert_eq!(3i, max);
+
+    rpc_call(proc(n) { println!("{:?}", n); });
+
+    each([1, 2, 3], |v: &int| {
+        println!("{:?}", v);
+    });
 }
